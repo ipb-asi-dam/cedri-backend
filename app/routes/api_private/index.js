63da7163fa85937ce1446e825393f2064366b4e0
Pedro Asseiro Rodrigues <p.rodrigues@hotmail.com>
2019-05-20 22:53:24 +0100

    Novas rotas

--- app/routes/api_private/index.js
+++ app/routes/api_private/index.js
@@ -4,4 +4,8 @@ const {isAdmin} = require('../../middleweres');
 router.use(isAdmin);
 router.use('/users', require('./admin/user'));
 router.use('/occupations', require('./admin/user/occupation.js'));
+router.use('/communications', require('./admin/communication'));
+router.use('/medias', require('./admin/communication/media.js'));
+router.use('/events', require('./admin/communication/event.js'));
+router.use('/news', require('./admin/communication/news.js'));
 module.exports = router;
