cb3c3b3f06b03698ca6cd8fa6451c9c8eaf3ae40
Pedro Asseiro Rodrigues <p.rodrigues@hotmail.com>
2019-06-22 11:40:38 +0100

    Alterações na tabela Communication

--- app/routes/api_private/admin/communication/index.js
+++ app/routes/api_private/admin/communication/index.js
@@ -1,27 +1,159 @@
 const router = require('express').Router()
 const models = require('../../../../models')
-const { check, validationResult } = require('express-validator/check')
-const Communication = models.communication
+const { param, check, validationResult } = require('express-validator/check')
+const Communication = models.publication
+const { hasPermission } = require('../../../../middleweres')
 
 router.post('/', [
+  hasPermission,
   check('name', 'Atributo name não pode ser nulo')
-    .exists(),
-
-  check('link', 'Atributo link não pode ser nulo')
+    .withMessage('name não pode ser nulo')
+    .toString(),
+  check('url')
     .exists()
-    .isURL().withMessage('Atributo link não é um URL')
-
-], async (req, res) => {
+    .withMessage('url não pode ser nulo')
+    .toString(),
+  check('type')
+    .exists()
+    .withMessage('type não pode ser nulo')
+    .toString()
+    .matches('^news$|^event$|^media$')
+    .withMessage('parâmetro type precisa ser (news ou event ou media)')
+], async (req, res, next) => {
   const errors = validationResult(req)
   if (!errors.isEmpty()) {
-    return res.status(422).json({ success: false, errors: errors.array() })
+    return res
+      .status(422)
+      .jsend
+      .fail({ errors: errors.array() })
   }
+  const comm = req.body
 
   try {
-    const communicationCreated = await Communication.create(req.body)
-    res.status(201).json({ success: true, data: communicationCreated })
+    const communication = await Communication.create(comm)
+    return res
+      .status(201)
+      .jsend
+      .success(communication)
   } catch (err) {
-    res.status(500).json({ success: false, msg: 'Erro ao criar Communication' })
+    return res
+      .status(500)
+      .jsend
+      .error({ message: 'Algo deu errado durante a criação da Communications' })
+  }
+})
+
+router.get('/', async (req, res, next) => {
+  try {
+    const communications = await Communication.scope('complete').findAll()
+    return res
+      .status(200)
+      .jsend
+      .success(communications)
+  } catch (err) {
+    return res
+      .status(500)
+      .jsend
+      .error({ message: 'Algo deu errado durante a listagem das Communications' })
+  }
+})
+
+router.get('/types/:type', [
+  param('type')
+    .matches('^news$|^event$|^media$')
+    .withMessage('parâmetro type precisa ser (news ou event ou media)')
+], async (req, res, next) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res
+      .status(422)
+      .jsend
+      .fail({ errors: errors.array() })
+  }
+  try {
+    const communications = await Communication.scope('complete').findAll({
+      where: {
+        type: req.params.type
+      }
+    })
+    return res
+      .status(200)
+      .jsend
+      .success(communications)
+  } catch (err) {
+    return res
+      .status(500)
+      .jsend
+      .error({
+        message: 'Algo deu errado durante a listagem das Communications do tipo ' + req.params.type
+      })
+  }
+})
+
+router.put('/:id', [
+  hasPermission,
+  check('name')
+    .optional(),
+  check('url')
+    .optional(),
+  check('context')
+    .optional(),
+  check('date')
+    .optional()
+    .isISO8601()
+    .withMessage('Formato date errado. Valor esperado (YYYY ou YYYY-MM ou YYYY-MM-DD'),
+  check('local')
+    .optional(),
+  check('type')
+    .optional()
+    .toString()
+    .matches('^news$|^event$|^media$')
+    .withMessage('parâmetro type precisa ser (news ou event ou media)')
+], async (req, res, next) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res
+      .status(422)
+      .jsend
+      .fail({ errors: errors.array() })
+  }
+  try {
+    const communication = await Communication.findByPk(req.params.id)
+    if (!communication) {
+      return res
+        .status(404)
+        .jsend
+        .fail({ message: 'Communication não encontrada!' })
+    }
+    await Communication.update(req.body,
+      {
+        where: {
+          id: communication.id
+        }
+      })
+
+    return res
+      .status(200)
+      .jsend
+      .success(await Communication.scope('complete').findByPk(communication.id))
+  } catch (err) {
+    console.log(err)
+    return res
+      .status(500)
+      .jsend
+      .error({ message: 'Algo deu errado durante o update da Communication' })
+  }
+})
+
+router.delete('/:id', async (req, res) => {
+  const id = req.params.id
+  try {
+    const communication = await Communication.destroy({
+      where: { id: id }
+    })
+    res.status(200).send({ success: true, data: communication })
+  } catch (err) {
+    return res.status(500).send({ success: false, msg: 'Erro ao apagar Communication.' })
   }
 })
 
