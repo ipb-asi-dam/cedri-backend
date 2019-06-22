6d2eda6f099cb9241fede43aab005d55a4915d2d
Guilhere Ianhez <guilherme.ianhez2@gmail.com>
2019-06-22 23:46:33 +0100

    removido rota para projects e posts

--- app/routes/api_public/index.js
+++ app/routes/api_public/index.js
@@ -4,7 +4,5 @@ router.use('/authenticate', require('./authenticate'))
 router.use('/recover', require('./recover'))
 router.use('/images', require('./images'))
 router.use('/statistics', require('./statistic'))
-router.use('/projects', require('./show/project'))
-router.use('/posts', require('./show/post'))
 
 module.exports = router
