c6e809773c68ba0ece4384d5ef3d30151d53afd5
Guilhere Ianhez <guilherme.ianhez2@gmail.com>
2019-06-22 23:27:22 +0100

    adiciona rota projeto e posts

--- app/routes/api_public/index.js
+++ app/routes/api_public/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router()
 
 router.use('/authenticate', require('./authenticate'))
-router.use('/recovery', require('./recovery'))
+router.use('/recover', require('./recover'))
 router.use('/images', require('./images'))
 router.use('/statistics', require('./statistic'))
+router.use('/projects', require('./show/project'))
+router.use('/posts', require('./show/post'))
 
 module.exports = router
