7731f5269abb12b9612f870f6bfe9d6ccd82c905
Guilhere Ianhez <guilherme.ianhez2@gmail.com>
2019-06-22 23:29:55 +0100

    renomeado rota recovery para recover

--- app/routes/api_public/recover/index.js
+++ app/routes/api_public/recover/index.js
@@ -4,7 +4,7 @@ const Investigator = models.investigator
 const env = process.env.NODE_ENV || 'development'
 const { API_SECRET } = require('../../../config/config.json')[env]
 const jwt = require('jsonwebtoken')
-const { check, validationResult } = require('express-validator/check')
+const { query, check, validationResult } = require('express-validator/check')
 const mailer = require('../../../config/global_modules/mailer-wrap')
 
 router.post('/', [
@@ -21,7 +21,7 @@ router.post('/', [
     return res.status(422).json({ success: false, errors: errors.array() })
   }
   try {
-    let investigador = await Investigator.findOne({
+    let investigador = await Investigator.scope('complete').findOne({
       where: {
         email: req.body.email
       }
@@ -42,7 +42,7 @@ router.post('/', [
     investigador.token = token
     try {
       await mailer.sendRecoveryEmail(investigador)
-      res.status(200)
+      return res.status(200)
         .jsend
         .success({ message: 'Email enviado com sucesso!' })
     } catch (err) {
@@ -56,6 +56,42 @@ router.post('/', [
       .jsend
       .error({ message: 'Não foi possivel concluir a solicitação de recovery' })
   }
+})
+
+router.put('/', [
+  check('password')
+    .exists()
+    .withMessage('Campo password não pode ser nulo')
+    .isLength({ min: 8, max: 255 })
+    .withMessage('Password precisa ter no minimo 8 e no máximo 255 caracteres'),
+  query('token')
+    .exists()
+    .withMessage('query token não pode ser nulo')
+], async (req, res) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ success: false, errors: errors.array() })
+  }
+  const token = req.query.token
+  jwt.verify(token, API_SECRET, async (error, decoded) => {
+    if (error) {
+      return res
+        .status(403)
+        .jsend.fail({ message: 'Token é inválido! Por favor, tente entrar novamente' })
+    }
+    const investigator = await Investigator.findByPk(decoded.id)
+    if (!investigator) {
+      return res
+        .status(404)
+        .jsend
+        .fail({ message: 'Investigador de id ' + decoded.id + '' })
+    }
+    await investigator.update(req.body, { fields: ['password'] })
+    return res
+      .status(200)
+      .jsend
+      .success({ message: 'Senha alterada com sucesso!' })
+  })
 })
 
 module.exports = router
