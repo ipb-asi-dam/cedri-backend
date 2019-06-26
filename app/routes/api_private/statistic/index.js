a76fd748896b6ab70c5d40aa613bde4cd4eefef7
Guilhere Ianhez <guilherme.ianhez2@gmail.com>
2019-06-26 02:43:06 +0100

    adiciona parametro showMy na rota statistics

--- app/routes/api_private/statistic/index.js
+++ app/routes/api_private/statistic/index.js
@@ -5,12 +5,24 @@ const { publication: Publication, these: These, award: Award, patent: Patent, so
 
 router.get('/outcomes', async (req, res, next) => {
   try {
-    const publications = (await Publication.findAndCountAll()).count
-    const theses = (await These.findAndCountAll()).count
-    const awards = (await Award.findAndCountAll()).count
-    const patents = (await Patent.findAndCountAll()).count
-    const softwares = (await Software.findAndCountAll()).count
-    const intelectualProp = patents + softwares
+    const showMy = (req.query.showMy === true || req.query.showMy === 'true')
+    const investigatorId = +req.user.id
+    let publications, theses, awards, patents, software
+    if (!showMy) {
+      publications = (await Publication.findAndCountAll()).count
+      theses = (await These.findAndCountAll()).count
+      awards = (await Award.findAndCountAll()).count
+      patents = (await Patent.findAndCountAll()).count
+      software = (await Software.findAndCountAll()).count
+    } else {
+      publications = (await Publication.findAndCountAll({ where: { investigatorId } })).count
+      theses = (await These.findAndCountAll({ where: { investigatorId } })).count
+      awards = (await Award.findAndCountAll({ where: { investigatorId } })).count
+      patents = (await Patent.findAndCountAll({ where: { investigatorId } })).count
+      software = (await Software.findAndCountAll({ where: { investigatorId } })).count
+    }
+
+    const intelectualProp = patents + software
     return res
       .status(200)
       .jsend
@@ -32,7 +44,14 @@ router.get('/outcomes', async (req, res, next) => {
 
 router.get('/awards', async (req, res, next) => {
   try {
-    const awards = (await Award.findAndCountAll()).count
+    const showMy = (req.query.showMy === true || req.query.showMy === 'true')
+    const investigatorId = +req.user.id
+    let awards
+    if (!showMy) {
+      awards = (await Award.findAndCountAll()).count
+    } else {
+      awards = (await Award.findAndCountAll({ where: { investigatorId } })).count
+    }
     return res
       .status(200)
       .jsend
@@ -51,15 +70,23 @@ router.get('/awards', async (req, res, next) => {
 
 router.get('/intellectual_properties', async (req, res, next) => {
   try {
-    const patents = (await Patent.findAndCountAll()).count
-    const softwares = (await Software.findAndCountAll()).count
+    const showMy = (req.query.showMy === true || req.query.showMy === 'true')
+    const investigatorId = +req.user.id
+    let patents, software
+    if (!showMy) {
+      patents = (await Patent.findAndCountAll()).count
+      software = (await Software.findAndCountAll()).count
+    } else {
+      patents = (await Patent.findAndCountAll({ where: { investigatorId } })).count
+      software = (await Software.findAndCountAll({ where: { investigatorId } })).count
+    }
     return res
       .status(200)
       .jsend
       .success({
         patents,
-        softwares,
-        total: patents + softwares
+        software,
+        total: patents + software
       })
   } catch (err) {
     console.log(err)
@@ -72,26 +99,51 @@ router.get('/intellectual_properties', async (req, res, next) => {
 
 router.get('/projects', async (req, res, next) => {
   try {
-    const international = (await Project.findAndCountAll(
-      {
-        where: {
-          type: 'international'
-        }
-      })).count
-
-    const national = (await Project.findAndCountAll(
-      {
-        where: {
-          type: 'national'
-        }
-      })).count
-
-    const other = (await Project.findAndCountAll(
-      {
-        where: {
-          type: 'other'
-        }
-      })).count
+    const showMy = (req.query.showMy === true || req.query.showMy === 'true')
+    const investigatorId = +req.user.id
+    let international, national, other
+    if (!showMy) {
+      international = (await Project.findAndCountAll(
+        {
+          where: {
+            type: 'international'
+          }
+        })).count
+      national = (await Project.findAndCountAll(
+        {
+          where: {
+            type: 'national'
+          }
+        })).count
+      other = (await Project.findAndCountAll(
+        {
+          where: {
+            type: 'other'
+          }
+        })).count
+    } else {
+      international = (await Project.findAndCountAll(
+        {
+          where: {
+            investigatorId,
+            type: 'international'
+          }
+        })).count
+      national = (await Project.findAndCountAll(
+        {
+          where: {
+            investigatorId,
+            type: 'national'
+          }
+        })).count
+      other = (await Project.findAndCountAll(
+        {
+          where: {
+            investigatorId,
+            type: 'other'
+          }
+        })).count
+    }
     return res
       .status(200)
       .jsend
@@ -112,30 +164,77 @@ router.get('/projects', async (req, res, next) => {
 
 router.get('/publications', async (req, res, next) => {
   try {
-    const books = (await Publication.findAndCountAll(
-      {
-        where: { type: 'b' }
-      })).count
-
-    const bookChapters = (await Publication.findAndCountAll(
-      {
-        where: { type: 'bc' }
-      })).count
-
-    const editorials = (await Publication.findAndCountAll(
-      {
-        where: { type: 'ed' }
-      })).count
-
-    const journals = (await Publication.findAndCountAll(
-      {
-        where: { type: 'j' }
-      })).count
-
-    const proceedings = (await Publication.findAndCountAll(
-      {
-        where: { type: 'p' }
-      })).count
+    const showMy = (req.query.showMy === true || req.query.showMy === 'true')
+    const investigatorId = +req.user.id
+    let books, bookChapters, editorials, proceedings, journals
+    if (!showMy) {
+      books = (await Publication.findAndCountAll(
+        {
+          where: {
+            type: 'b'
+          }
+        })).count
+      bookChapters = (await Publication.findAndCountAll(
+        {
+          where: {
+            type: 'bc'
+          }
+        })).count
+      editorials = (await Publication.findAndCountAll(
+        {
+          where: {
+            type: 'e'
+          }
+        })).count
+      proceedings = (await Publication.findAndCountAll(
+        {
+          where: {
+            type: 'p'
+          }
+        })).count
+      journals = (await Publication.findAndCountAll(
+        {
+          where: {
+            type: 'j'
+          }
+        })).count
+    } else {
+      books = (await Publication.findAndCountAll(
+        {
+          where: {
+            investigatorId,
+            type: 'b'
+          }
+        })).count
+      bookChapters = (await Publication.findAndCountAll(
+        {
+          where: {
+            investigatorId,
+            type: 'bc'
+          }
+        })).count
+      editorials = (await Publication.findAndCountAll(
+        {
+          where: {
+            investigatorId,
+            type: 'e'
+          }
+        })).count
+      proceedings = (await Publication.findAndCountAll(
+        {
+          where: {
+            investigatorId,
+            type: 'p'
+          }
+        })).count
+      journals = (await Publication.findAndCountAll(
+        {
+          where: {
+            investigatorId,
+            type: 'j'
+          }
+        })).count
+    }
 
     return res
       .status(200)
@@ -159,19 +258,40 @@ router.get('/publications', async (req, res, next) => {
 
 router.get('/theses', async (req, res, next) => {
   try {
-    const mscs = (await These.findAndCountAll(
-      {
-        where: {
-          type: 'msc'
-        }
-      })).count
+    const showMy = (req.query.showMy === true || req.query.showMy === 'true')
+    const investigatorId = +req.user.id
+    let mscs, phds
+    if (!showMy) {
+      mscs = (await These.findAndCountAll(
+        {
+          where: {
+            type: 'msc'
+          }
+        })).count
 
-    const phds = (await These.findAndCountAll(
-      {
-        where: {
-          type: 'phd'
-        }
-      })).count
+      phds = (await These.findAndCountAll(
+        {
+          where: {
+            type: 'phd'
+          }
+        })).count
+    } else {
+      mscs = (await These.findAndCountAll(
+        {
+          where: {
+            investigatorId,
+            type: 'msc'
+          }
+        })).count
+
+      phds = (await These.findAndCountAll(
+        {
+          where: {
+            investigatorId,
+            type: 'phd'
+          }
+        })).count
+    }
 
     return res
       .status(200)
