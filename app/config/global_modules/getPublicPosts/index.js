00c242b4e0b21b364f13571083ce5bf53a6c525e
Guilhere Ianhez <guilherme.ianhez2@gmail.com>
2019-07-04 11:10:00 +0100

    adicionando paginação em posts publicos

--- app/config/global_modules/getPublicPosts/index.js
+++ app/config/global_modules/getPublicPosts/index.js
@@ -1,9 +1,14 @@
 module.exports = async (req, Model, optionsObj) => {
-  let elements
-  if (!optionsObj) {
-    elements = await Model.scope('public').findAll()
-  } else {
-    elements = await Model.scope('public').findAll(optionsObj)
-  }
+  const limit = req.query.limit
+  const page = req.query.page
+  const offset = limit * (page - 1)
+  if (!optionsObj) optionsObj = {}
+
+  optionsObj.limit = limit
+  optionsObj.page = page
+  optionsObj.offset = offset
+
+  const elements = await Model.scope('public').findAll(optionsObj)
+
   return elements
 }
